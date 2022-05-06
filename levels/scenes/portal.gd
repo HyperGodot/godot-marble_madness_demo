702a4808cea8245b745c68ec7e20a4514e1347aa
plankatron <fleeky@gmail.com>
2022-05-05 23:01:59 -0400

    adding in game portal

---
+++ levels/scenes/portal.gd
@@ -0,0 +1,19 @@
+extends Spatial
+
+export var portal_destination : String = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func tryGameChange(gameChangeName : String, sendGossip : bool, playerNode):
+	OS.execute(OS.get_executable_path(), ["--main-pack", portal_destination], false)
+	get_tree().quit()
+
+func _on_CollisionShape_gameplay_entered():
+	pass # Replace with function body.
+
+func _on_Hitbox_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if(body is RigidBody):
+		tryGameChange(portal_destination, true, body)
+		#print("blah")
